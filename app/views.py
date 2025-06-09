1f518051c34322be299ac2c34f1bece3e13a18ea
emilia <majdysola@gmail.com>
2025-06-10 00:24:21 +0200

    2025-06-10 EM

--- app/views.py
+++ app/views.py
@@ -63,7 +63,12 @@ def extract():
                 next_page= None
                 print("Brak kolejnej strony")
         else: print(response.status_code)
-
+    if not os.path.exists("./app/data"):
+        os.mkdir("./app/data")
+    if not os.path.exists("./app/data/opinions"):
+        os.mkdir("./app/data/opinions")
+    with open(f"./app/data/opinions/{product_id}.json", "w", encoding="UTF-8") as jf:
+        json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
     opinions = pd.DataFrame.from_dict(all_opinions)
     opinions.stars = opinions.stars.apply(lambda s: s.split("/")[0].replace(",", ".")).astype(float)
     opinions.useful= opinions.useful.astype(int)
@@ -79,7 +84,7 @@ def extract():
         "avarage_stars":float(opinions.stars.mean()),
         "pros":opinions.pros.explode().dropna().value_counts().to_dict(),
         "cons":opinions.cons.explode().dropna().value_counts().to_dict(),
-        "recommendations": opinions.recommandation.value_counts(dropna=False).reindex(['Nie polecam', 'Polecam',  None],fill_value=0 ).to_dict()
+        "recommendations": opinions.recommandation.value_counts(dropna=False).reindex(['Nie polecam', 'Polecam',  None],fill_value=0 ).to_dict(),
         
   
     }
@@ -102,7 +107,7 @@ def products():
         with open(f"./app/data/products/{filename}", "r", encoding="UTF-8") as jf:
             product = json.load(jf)
             products_list.append(product)
-    return render_template("products.html")
+    return render_template("products.html", products=products_list)
 
 @app.route("/author")
 def author():
@@ -113,8 +118,7 @@ def product(product_id):
     product_name=request.args.get('product_name')
     with open(f"./app/data/opinions/{product_id}.json", "r",encoding="UTF-8") as jf:
         opinions=json.load(jf)
-    return render_template("product.html", product_id=product_id, product_name=product_name, opinions=opinions)
-
+    return render_template("product.html", product_id=product_id, product_name=product_name, opinions=opinions) 
 @app.route("/charts/<product_id>")
 
 def charts(product_id):
@@ -123,18 +127,16 @@ def charts(product_id):
         os.mkdir("./app/static/iamges")
     if not os.path.exists("./app/static/images/charts"):
         os.mkdir("./app/static/images/charts")
-    with open(f"./app/data/products/{product_id}.json", "w", encoding="UTF-8") as jf:
-        json.dump(stats, jf, indent=4, ensure_ascii=False)
     with open(f"./app/data/products/{product_id}.json", "r", encoding="UTF-8") as jf:
         stats=json.load(jf)
     recommendations=pd.Series(stats['recommendations'])
     recommendations.plot.pie(
     label="",
-    title="rozklad rekomendacji w opiniach o produkcie",
+    title=f"rozklad rekomendacji w opiniach o produkcie {product_id}",
     labels=['nie polecam', 'polecam', 'nie mam zdania'],
     colors=["pink", "orange", "yellow"],
     autopct="%1.1f%%"
 )
-    plt.savefig(f"./app/static/images/charts/{stats['product_id']}, pie.png")
+    plt.savefig(f"./app/static/images/charts/{stats['product_id']}_pie.png")
     plt.close()
     return render_template("charts.html", product_id=product_id, product_name=stats['product_name'])
